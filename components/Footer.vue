ad44a64fa906d2984d846d6e933c7ae0870f57ea
DONG WOONG OH <integral5137@gmail.com>
2021-10-10 20:39:03 +0900

    Set up to base component

---
+++ components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer class='text-center'>
+    <v-card-text>
+      <v-btn v-for='icon in icons' :key='icon' class='mx-4' icon>
+        <v-icon size='24px'>
+          {{ icon }}
+        </v-icon>
+      </v-btn>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-text class='white--text'>
+      {{ new Date().getFullYear() }} — <strong>©Buy the Label</strong>
+    </v-card-text>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data: () => ({
+    icons: ['mdi-home', 'mdi-email', 'mdi-calendar', 'mdi-delete'],
+    items: ['default', 'absolute', 'fixed'],
+    variant: 'default'
+  }),
+  computed: {
+    localAttrs() {
+      const attrs = {}
+
+      if (this.variant === 'default') {
+        attrs.absolute = false
+        attrs.fixed = false
+      } else {
+        attrs[this.variant] = true
+      }
+      return attrs
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>Index</div>
+  <div>Login</div>
 </template>
